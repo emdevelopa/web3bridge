7f89b2da77ed418aebd31ad426c3be58b933afd8
emdevelopa <gyimahsunday@gmail.com>
2026-02-19 23:42:52 +0100

    day-4 session

--- day-3/assignment/schoolManagement.sol
+++ day-3/assignment/schoolManagement.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract SchoolManagement {
-
+contract SchoolSystem{
     address public owner;
 
     constructor() {
@@ -36,79 +35,9 @@ contract SchoolManagement {
     mapping(uint => Staff) public staffs;
 
     uint[] public studentIds;
-    uint[] public staffIds;
+    uint[] public staffCounter;
 
     uint public studentCounter;
     uint public staffCounter;
 
- 
-    function registerStudent(string memory _name, uint _level) public payable {
-
-        require(_level >= 100 && _level <= 400, "Invalid level");
-
-        studentCounter++;
-
-        uint fee = getFeeByLevel(_level);
-
-        require(msg.value == fee, "Incorrect school fee");
-
-        students[studentCounter] = Student(
-            studentCounter,
-            _name,
-            _level,
-            fee,
-            true,
-            block.timestamp
-        );
-
-        studentIds.push(studentCounter);
-    }
-
- 
-    function registerStaff(string memory _name, string memory _role, uint _salary) public onlyOwner {
-
-        staffCounter++;
-
-        staffs[staffCounter] = Staff(
-            staffCounter,
-            _name,
-            _role,
-            _salary,
-            false,
-            0
-        );
-
-        staffIds.push(staffCounter);
-    }
-
-     
-    function payStaff(uint _staffId) public payable onlyOwner {
-
-        Staff storage s = staffs[_staffId];
-
-        require(!s.isPaid, "Already paid");
-        require(msg.value == s.salary, "Incorrect salary amount");
-
-        s.isPaid = true;
-        s.paymentTimestamp = block.timestamp;
-    }
- 
-    function getStudent(uint _studentId) public view returns (Student memory) {
-        return students[_studentId];
-    }
-
- 
-    function getAllStaff() public view returns (uint[] memory) {
-        return staffIds;
-    }
-
-     
-    function getFeeByLevel(uint _level) internal pure returns (uint) {
-        if (_level == 100) return 1 ether;
-        if (_level == 200) return 2 ether;
-        if (_level == 300) return 3 ether;
-        if (_level == 400) return 4 ether;
-
-        revert("Invalid level");
-    }
 }
---
+++ day4/class_activity/erc20.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IERC20{
+    function name() external view returns (string);
+    function symbol() external view returns (string);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining)
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value)
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value)
+
+
+}
+
+contract ERC20{
+    string constant TOKEN_NAME = "WEB3CXIV";
+    string constant TOKEN_SYMBOL = "W3CXIV";
+    uint8 constant TOKEN_DECIMAL = 18;
+    uint256 constant TOTAL_SUPPLY = 2_000_000_000_000_000_000_000;
+
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowances;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    function name() external view returns (string){
+        return TOKEN_NAME;
+    }
+
+    function symbol() external view returns (string){
+        return TOKEN_SYMBOL;
+    }
+
+    function decimals() external view returns (uint8){
+        return  TOKEN_DECIMAL;
+    }
+
+    function totalSupply() external view returns (uint256){
+        return TOTAL_SUPPLY;
+    }
+
+    function balanceOf(address _owner) external view returns (uint256 balance){
+       return balances[_owner];
+    }
+
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining){
+        remaining = allowances[_owner][_spender];
+
+        return remaining;
+    }
+
+    function transfer(address _to, uint256 _value) external returns (bool success){
+        require(balances[msg.sender] >= _value, "Insufficient balance");
+        
+        balances[msg.sender] = balances[msg.sender] - _value; 
+        balances[_to] = balances[_to] + _value; 
+
+        return true
+    }
+
+     function approve(address _spender, uint256 _value) external returns (bool success){
+        require(balances[msg.sender] >= _value, "Insufficient Balance");
+
+        allowances[msg.sender][_sender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+     }
+
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success){
+        require(balance[_from] >= _value, "Insufficient Balance From owner");
+        require(allowances[_from][msg.sender] >= _value, "amount entered is not allowed to be spent");
+
+        balances[_from] = balances[_from] - _value;
+        balances[_to] = balances[_to] + _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+
+
+    
+
+
+}
+
