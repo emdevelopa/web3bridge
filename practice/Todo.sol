16a5b3caa6acf9ad23e705642f63c029e6b101c9
emdevelopa <gyimahsunday@gmail.com>
2026-02-21 13:34:56 +0000

    feat: added Todo contract

---
+++ practice/Todo.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Todo {
+    // createTodo
+    // deleteTodo
+    // UpdateTodo
+    // markTodoCompleted
+    // markTodoUncompleted
+    // getAllTodos
+
+    struct TodoFields {
+        uint8 id;
+        string title;
+        bool completed;
+        uint timeCompleted;
+    }
+
+    uint[] nums = [1,2,3,4,5,6,9,10];
+
+    TodoFields[] Todos;
+    uint8 todoId;
+
+    // For testing Swap and Popu
+    // function SwapItemAndPop() external returns(uint[] memory){
+    //     nums[2] = nums[6];
+
+    //     return nums;
+    // }
+
+    function getAllTodos() external view returns (TodoFields[] memory) {
+        return Todos;
+    }
+
+    function createTodo(string memory _title) external {
+        todoId = todoId + 1;
+
+        /* need to create a struct first and then push  the struct to the array too store it in the blockchain state */
+
+        TodoFields memory todo = TodoFields({
+            id: todoId,
+            title: _title,
+            completed: false,
+            timeCompleted: 0
+        });
+
+        Todos.push(todo);
+    }
+
+    function markCompleted(uint8 _id) external {
+        for (uint i = 0; i < Todos.length; i++) {
+            if (Todos[i].id == _id) {
+                Todos[i].completed = true;
+                Todos[i].timeCompleted = block.timestamp;
+            }
+        }
+    }
+    function markUnCompleted(uint8 _id) external {
+        for (uint i = 0; i < Todos.length; i++) {
+            if (Todos[i].id == _id) {
+                Todos[i].completed = false;
+                Todos[i].timeCompleted = 0;
+            }
+        }
+    }
+
+    function deleteTodo(uint _id) external view {
+         for (uint i = 0; i < Todos.length; i++) {
+            if (Todos[i].id == _id) {
+               
+            }
+        }
+    }
+
+    function updateTodo(uint _id, string memory _title) external {
+        for (uint i = 0; i < Todos.length; i++) {
+            if (Todos[i].id == _id) {
+                require(
+                    !Todos[i].completed,
+                    "Todo completed, can't update it.."
+                );
+                Todos[i].title = _title;
+                Todos[i].timeCompleted = 0;
+            }
+        }
+    }
+}
